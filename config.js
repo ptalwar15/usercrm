50b605a1bd31d0287458a1e02e88dd18e35894b8
Pradeep Talwar <ptal528@yahoo.com>
2015-12-15 14:58:16 -0500

    added remote DB

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
 	'port': process.env.PORT || 3000,
-	'database':"mongodb://localhost:27017/crm",
+	'database':"mongodb:ptalwar15:Coolit12@ds047901.mongolab.com:47901/usercrm",
 	'secret':'ilovescotchscotchyscotchscotch'
 }
+
+
+        //
+        //local db : "mongodb://localhost:27017/crm"
